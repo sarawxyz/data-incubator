00bcc9bdb2c1d348e56a61d6083c8bd0daac653e
unknown <sweinste@DC073533.idir.BCGOV>
2015-01-28 13:58:25 -0800

    added user_freq

--- DI-Q2.R
+++ DI-Q2.R
@@ -13,6 +13,11 @@ hits$time <- strptime(hits$time, "%Y-%m-%d %H:%M:%S")
 hits$user <- as.factor(hits$user)
 hits$category <- as.factor(hits$category)
 
+###table of # visits per user
+user_freq <-as.data.frame(table(hits$user))
+
+
+
 ###Question 1
 ###Mean # of seconds between first & last page visits, when visits >1
 
@@ -24,4 +29,5 @@ seconds <- difftime(max(hits$time), min(hits$time), units = "secs")
 
 ###Question 3
 ###Average # page visits, when visits > 1
+
 
